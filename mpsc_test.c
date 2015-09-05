a93858c7d7d20c4e6bf04f33bf9a23ea56df92dd
Daniel Bittman <danielbittman1@gmail.com>
2015-09-04 17:16:20 -0700

    cleanup

--- mpsc_test.c
+++ mpsc_test.c
@@ -12,10 +12,12 @@ _Atomic int amount_produced = ATOMIC_VAR_INIT(0);
 _Atomic int amount_consumed = ATOMIC_VAR_INIT(0);
 _Atomic bool done = ATOMIC_VAR_INIT(false);
 
+#define NUM_ITEMS 2000
+#define NUM_THREADS 2001
+#define CAPACITY 50000000
 void *producer_main(void *x)
 {
-	int num = 20;
-	for(int i=0;i<num;i++) {
+	for(int i=0;i<NUM_ITEMS;i++) {
 		bool r = mpscq_enqueue(queue, x);
 		if(r) {
 			atomic_fetch_add(&amount_produced, 1);
@@ -49,13 +51,15 @@ int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	int num_producers = 20;
+	int num_producers = NUM_THREADS-1;
 	pthread_t producers[num_producers];
 	pthread_t consumer;
 
-	queue = mpscq_create(NULL, 8000);
+	queue = mpscq_create(NULL, CAPACITY);
 
-	fprintf(stderr, ":: LOCK FREE? %d %d %d %d\n", ATOMIC_INT_LOCK_FREE, ATOMIC_LONG_LOCK_FREE, ATOMIC_POINTER_LOCK_FREE, ATOMIC_LLONG_LOCK_FREE);
+	fprintf(stderr, ":: LOCK FREE? int=%d long=%d ptr=%d llong=%d\n",
+			ATOMIC_INT_LOCK_FREE, ATOMIC_LONG_LOCK_FREE,
+			ATOMIC_POINTER_LOCK_FREE, ATOMIC_LLONG_LOCK_FREE);
 
 	pthread_create(&consumer, NULL, consumer_main, NULL);
 	for(long i=0;i<num_producers;i++) {
--- mpscq.h
+++ mpscq.h
@@ -17,12 +17,30 @@ struct mpscq {
 	int flags;
 };
 
-
+/* create a new mpscq. If n == NULL, it will allocate
+ * a new one and return it. If n != NULL, it will
+ * initialize the structure that was passed in. */
 struct mpscq *mpscq_create(struct mpscq *n, size_t capacity);
+
+/* enqueue an item into the queue. Returns true on success
+ * and false on failure (queue full). This is safe to call
+ * from multiple threads */
 bool mpscq_enqueue(struct mpscq *q, void *obj);
+
+/* dequeue an item from the queue and return it.
+ * THIS IS NOT SAFE TO CALL FROM MULTIPLE THREADS.
+ * Returns NULL on failure, and the item it dequeued
+ * on success */
 void *mpscq_dequeue(struct mpscq *q);
+
+/* get the number of items in the queue currently */
 size_t mpscq_count(struct mpscq *q);
+
+/* get the capacity of the queue */
 size_t mpscq_capacity(struct mpscq *q);
+
+/* destroy a mpscq. Frees the internal buffer, and
+ * frees q if it was created by passing NULL to mpscq_create */
 void mpscq_destroy(struct mpscq *q);
 
 
